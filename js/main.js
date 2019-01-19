587121b1a432847e1af27c37e7e9e993626a6123
SparkeEcrash <jpark121989@gmail.com>
2019-01-18 19:20:26 -0800

    uploading myPortfolio template

---
+++ js/main.js
@@ -0,0 +1,46 @@
+$(document).ready(initialize)
+
+function initialize() {
+    //navbar-toggler
+    $('.navbar-toggler').click(function() {
+        $('.navbar-toggler').toggleClass('change');
+    })
+    //adding background and fixed
+    $(window).scroll(function() {
+        let position = $(this).scrollTop();
+        console.log(position);
+        if(position >= 149) {
+            $('.navbar').addClass('navbar-background');
+            $('.navbar').addClass('fixed-top');
+        } else {
+            $('.navbar').removeClass('navbar-background');
+            $('.navbar').removeClass('fixed-top');
+        }
+    })
+    //smooth scroll
+
+    $('.nav-item a').click(function(event){
+        event.preventDefault()
+        let target = $(this).attr('href');
+        $('html, body').stop().animate({
+            scrollTop:$(target).offset().top
+        },1500)
+    })
+
+    //init Isotope
+    let $grid = $('.grid').isotope({
+        //options
+    });
+    //filter items on button click
+    $('.filter-button-group').on('click', 'button', function() {
+        let filterValue = $(this).attr('data-filter');
+        $grid.isotope({
+            filter: filterValue
+        });
+    });
+
+    //load images first to begin isotope filtering
+    $grid.imagesLoaded().progress( function() {
+        $grid.isotope('layout');
+      });
+}
